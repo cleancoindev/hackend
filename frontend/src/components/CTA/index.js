9b199ba19585953fd73189a8e93572d8279c5339
Anish Agnihotri <contact@anishagnihotri.com>
2019-09-10 21:56:55 -0400

    Add landing cta

--- frontend/src/components/CTA/index.js
+++ frontend/src/components/CTA/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import macbook from './macbook.svg';
 import './index.css';
 
 export default class CTA extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			adjectives: ['simply', 'tremendously', 'effectively', 'quickly', 'nicely', 'superbly', 'collaboratively', 'conveniently'],
+			adjectiveInitial: 'easily'
+		}
+	}
+
 	render() {
 		return(
 			<div className="section cta">
-				<div className="pageItem">
-					<div>
-						<p>Something</p>
+				<div>
+					<div className="pageItem">
+						<div>
+							<h1>Manage hackathons<br /><a href='javascript:void(0)'>conveniently</a>.</h1>
+							<img src={macbook} alt='Hackend dashboard demo' />
+						</div>
 					</div>
 				</div>
 			</div>
