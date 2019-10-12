d4cd5244b329f6026f6edb4587e0c815c9db4822
Anish Agnihotri <contact@anishagnihotri.com>
2019-10-11 21:54:22 -0400

    wip

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import TextLoop from "react-text-loop";
+import CountUp from 'react-countup';
+
+import airtable from './temp_images/airtable.png';
+import drive from './temp_images/drive.png';
+import fathom from './temp_images/fathom.png';
+import instagram from './temp_images/instagram.png';
+import intercom from './temp_images/intercom.png';
+import porkbun from './temp_images/porkbun.png';
+import printfection from './temp_images/printfection.png';
+import slack from './temp_images/slack.png';
+import trello from './temp_images/trello.png';
+
 import './index.css';
 
 export default class Landing extends React.Component {
@@ -130,13 +142,29 @@ export default class Landing extends React.Component {
 					</div>
 				</div>
 				<div className="integrations">
-
+					<img src={airtable} id="airtable"/>
+					<img src={drive} id="drive"/>
+					<img src={fathom} id="fathom"/>
+					<img src={instagram} id="instagram"/>
+					<img src={porkbun} id="porkbun" className="specific"/>
+					<img src={printfection} id="printfection"/>
+					<img src={slack} id="slack"/>
+					<img src={trello} id="trello" className="specific"/>
+					<div>
+						<h1>Over <span><CountUp start={10} end={45} duration={3}/></span> integrations.</h1>
+						<p>Connect with your favourite tools and start collaborating together.</p>
+					</div>
 				</div>
 				<div className="testemonials">
 
 				</div>
 				<div className="footer">
+					<div>
 
+					</div>
+					<div>
+
+					</div>
 				</div>
 			</div>
 		);
